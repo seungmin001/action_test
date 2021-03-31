389ddb7414365d49a8cca64f91e884e5fbd7b513
seungmin001 <miinnie9808@gmail.com>
2021-03-31 22:50:56 +0900

    TEST-1 jira test

--- src/main/java/ForValid/ForAction.java
+++ src/main/java/ForValid/ForAction.java
@@ -3,5 +3,6 @@ package ForValid;
 public class ForAction {
     public static void main(String[] args) {
         System.out.println("hello java");
+        System.out.println("jira confirm");
     }
 }
