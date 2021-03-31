58582e0e041e13c529239cb38fe503a717c6f84b
seungmin001 <miinnie9808@gmail.com>
2021-03-31 23:47:14 +0900

    TEMP-1 delete package to change name

--- src/main/java/ForValid/ForAction.java
+++
@@ -1,8 +0,0 @@
-package ForValid;
-
-public class ForAction {
-    public static void main(String[] args) {
-        System.out.println("hello java");
-        System.out.println("jira TEMP-1 issue connect confirm");
-    }
-}
